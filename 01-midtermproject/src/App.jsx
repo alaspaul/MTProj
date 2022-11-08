0f22ade5fc6e4e9f2c95aaf9bb07756721a12aa3
paul <paul4145irvin@gmail.com>
2022-11-08 20:37:24 +0800

    Starting From scratch

---
+++ 01-midtermproject/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <main>
+      <div className='Body'>
+        <div className='Header'>
+          <h1>Grocery Bud</h1>
+        </div>
+       
+        
+        
+        
+        
+        
+        
+        
+        </div>
+    </main>
+  )
+}
+
+export default App

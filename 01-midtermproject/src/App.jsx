9dc7adaf179922a8587f43054b65d93982dfd154
paul <paul4145irvin@gmail.com>
2022-11-09 20:11:11 +0800

    adding edit button and finishing the add button

--- 01-midtermproject/src/App.jsx
+++ 01-midtermproject/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 
+import {nanoid} from 'nanoid'
 import './App.css'
 import Data from './Grocery-Data.json'
 
@@ -10,31 +11,74 @@ function App() {
 
   
 const[list, setList] = useState(Data);
+const[addFormData, setAddFormData] = useState({
+  item:''
+});
+
+const [editItemId, setEditItemId] = useState(null);
+
+
+const addItem =(event) => {
+  event.preventDefault();
+
+  const fieldName = event.target.getAttribute('name');
+  const fieldValue = event.target.value;
+
+  const newFormData = { ...addFormData};
+
+  newFormData[fieldName] = fieldValue;
+
+  setAddFormData(newFormData);
+}
+
+
+  const addList = (event) => {
+    event.preventDefault();
+
+    const newList ={
+      id: nanoid(),
+      item: addFormData.item
+    }
+
+
+    const newLists = [...list, newList];
+    setList(newLists);
+  }
  
+    const editList = (event, list) =>{
+      event.preventDefault();
+
+      setEditItemId(list.id);
+    }
   
 
   return (
     <main>
       <div className='App-container'>
+    
         <table>
           <thead>
             <tr>
               <th>
                 Grocery Bud
+                <form onSubmit={addList}>
+            <input type="text" name="item" required="required" placeholder="e.g. eggs" onChange={addItem} />
+
+           <button type="submit">Add</button>
+          </form>
               </th>
             </tr>
           </thead>
-          <form>
-            <input type="text" name="item" required="required" placeholder="e.g. eggs">
-
-            </input>
-          </form>
+          
           <tbody>
 
             {list.map((list) =>( 
               <tr>
                 <td>
                  {list.item}
+                </td>
+                <td>
+                  <button type="button" onClick={(event)=> editList(event, list)}>edit</button>
                 </td>
               </tr>
                 ))}

95a1d139fb1581a4156b9382816cc34ecdbdb945
paul <paul4145irvin@gmail.com>
2022-11-10 19:31:45 +0800

    creating list, app and alerts

---
+++ 01-midtermproject/src/Alert.jsx
@@ -0,0 +1,19 @@
+import React, {useEffect} from "react";
+
+
+const Alert = () => {
+    
+    return
+            <main>
+                <h2>alert Component</h2>
+            </main>
+    
+
+
+    
+}
+
+
+
+
+    export default Alert
